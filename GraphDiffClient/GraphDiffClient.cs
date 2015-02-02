f62d3d4be08dc50e87300a8c3a94d229df476898
Anders Østhus <anders.osthus@outlook.com>
2015-02-02 12:06:22 +0100

    Refactoring of call methods
    Also added public Interface and GetNextPageAsync

--- GraphDiffClient/GraphDiffClient.cs
+++ GraphDiffClient/GraphDiffClient.cs
@@ -10,78 +10,109 @@ using Newtonsoft.Json.Linq;
 
 namespace GraphDiffClient
 {
-	public class GraphDiffClient
-	{
-		private readonly HttpClient _httpClient;
-		private readonly string _tenantId;
-		private readonly Func<Task<string>> _tokenRetriever;
-		private string _accessToken;
-	    private string _nextLink;
+    public class GraphDiffClient : IGraphDiffClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _tenantId;
+        private readonly Func<Task<string>> _tokenRetriever;
+        private string _accessToken;
+        private string _nextLink;
 
-		public GraphDiffClient(Func<Task<string>> tokenRetriever, string tenantId)
-		{
-			_httpClient = new HttpClient();
-			_tenantId = tenantId;
-			_tokenRetriever = tokenRetriever;
-		}
+        public GraphDiffClient(Func<Task<string>> tokenRetriever, string tenantId)
+        {
+            _httpClient = new HttpClient();
+            _tenantId = tenantId;
+            _tokenRetriever = tokenRetriever;
+        }
 
-	    public async Task<DiffResponse> GetObjectsAsync()
-	    {
+        public async Task<DiffResponse> GetObjectsAsync()
+        {
+            var queryParams = GenerateQueryParams(null);
+            var requestUri =
+                new Uri(string.Format("https://graph.windows.net/{0}/directoryObjects", _tenantId)).AddQueryParameters(
+                    queryParams);
+
+            var data = await CallAdAsync(requestUri).ConfigureAwait(false);
+            return data;
+        }
+
+        public async Task<DiffResponse> GetNextPageAsync()
+        {
+            if (string.IsNullOrEmpty(_nextLink))
+                throw new Exception("You cannot call this when there's no NextPageLink!");
+
+            var requestUri = new Uri(_nextLink);
+            var data = await CallAdAsync(requestUri).ConfigureAwait(false);
+            return data;
+        }
+
+        private async Task<DiffResponse> CallAdAsync(Uri requestUri)
+        {
             if (string.IsNullOrEmpty(_accessToken))
                 _accessToken = await _tokenRetriever().ConfigureAwait(false);
 
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
 
-            var queryParams = GenerateQueryParams(null);
-            var requestUri = new Uri(string.Format("https://graph.windows.net/{0}/directoryObjects", _tenantId)).AddQueryParameters(queryParams);
+            var result = await ExecuteHttpRequestAsync(requestUri);
+            var stringResult = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var data = ParseResponse(stringResult);
+
+            _nextLink = data.HasMorePages
+                ? data.NextPage
+                : string.Empty;
+
+            return data;
+        }
+
+        private async Task<HttpResponseMessage> ExecuteHttpRequestAsync(Uri requestUri)
+        {
             var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             request.Headers.AcceptCharset.Add(new StringWithQualityHeaderValue("UTF-8"));
 
             var result = await _httpClient.SendAsync(request).ConfigureAwait(false);
-            var stringResult = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return result;
+        }
 
+        private static DiffResponse ParseResponse(string stringResult)
+        {
             var data = JsonConvert.DeserializeObject<DiffResponse>(stringResult);
-
-	        var asJobject = JObject.Parse(stringResult);
-	        foreach (var change in asJobject["value"])
-	        {
+            var asJobject = JObject.Parse(stringResult);
+            foreach (var change in asJobject["value"])
+            {
                 switch (change["odata.type"].ToString())
-	            {
+                {
                     case "Microsoft.DirectoryServices.User":
                         data.Users.Add(change.ToObject<User>());
-	                    break;
+                        break;
                     case "Microsoft.DirectoryServices.Group":
                         data.Groups.Add(change.ToObject<Group>());
-	                    break;
+                        break;
                     case "Microsoft.DirectoryServices.Contact":
-	                    break;
+                        break;
                     case "Microsoft.DirectoryServices.DirectoryLinkChange":
                         data.DirectoryLinkChanges.Add(change.ToObject<DirectoryLinkChange>());
-	                    break;
-	            }
-	        }
-
-	        if (data.HasMorePages)
-	            _nextLink = data.NextPage;
-
+                        break;
+                }
+            }
             return data;
-	    }
+        }
 
-		private static Dictionary<string, string> GenerateQueryParams(List<string> selectList)
-		{
-			var queryParams = new Dictionary<string, string>
-			{
-				{"api-version", "1.5"},
-				{"deltaLink", ""},
-			};
+        private static Dictionary<string, string> GenerateQueryParams(List<string> selectList)
+        {
+            var queryParams = new Dictionary<string, string>
+            {
+                {"api-version", "1.5"},
+                {"deltaLink", ""},
+            };
 
-			if (selectList == null || !selectList.Any()) return queryParams;
+            if (selectList == null || !selectList.Any()) return queryParams;
 
-			var selectFilter = selectList.Aggregate((s1, s2) => s1 + "," + s2);
-			queryParams["$select"] = selectFilter;
+            var selectFilter = selectList.Aggregate((s1, s2) => s1 + "," + s2);
+            queryParams["$select"] = selectFilter;
 
-			return queryParams;
-		}
-	}
+            return queryParams;
+        }
+    }
 }
---
+++ GraphDiffClient/IGraphDiffClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using GraphDiffClient.Models;
+
+namespace GraphDiffClient
+{
+    public interface IGraphDiffClient
+    {
+        Task<DiffResponse> GetObjectsAsync();
+        Task<DiffResponse> GetNextPageAsync();
+    }
+}
